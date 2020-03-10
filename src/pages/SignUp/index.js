f697c6d76cceb0671ffa67997d93d24205592796
Emerson O da Hora <emersondahora@gmail.com>
2020-03-10 20:11:17 -0300

    Validaçoes

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Form, Input } from "@rocketseat/unform";
+import * as Yup from "yup";
 
 import logo from "~/assets/image/logo.svg";
 // import { Container } from './styles';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required("O nome é obrigatório"),
+  email: Yup.string()
+    .email("Insira um e-mail válido")
+    .required("O e-mail é obrigatório"),
+  password: Yup.string()
+    .required("A senha é obrigatória")
+    .min(6, "No mínimo 6 caracteres")
+});
 
 export default function SignUp() {
   function handleSubmit(data) {
@@ -13,7 +24,7 @@ export default function SignUp() {
     <>
       <img src={logo} alt="GoBarBer" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={handleSubmit} schema={schema}>
         <Input name="name" placeholder="Nome Completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Seu e-mail" />
