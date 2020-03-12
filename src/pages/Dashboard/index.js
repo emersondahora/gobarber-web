5ef3202373ca05303b32b869f7fbb1f15359a39c
Emerson O da Hora <emersondahora@gmail.com>
2020-03-12 13:34:49 -0300

    Estilizaçao do Dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
-// import { Container } from './styles';
+import { Container, Time } from "./styles";
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} />
+        </button>
+        <strong>31 de Maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} />
+        </button>
+      </header>
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Diego Ferndandes</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Emerson Oliveira</span>
+        </Time>
+        <Time>
+          <strong>12:00</strong>
+          <span>Emerson Oliveira</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
