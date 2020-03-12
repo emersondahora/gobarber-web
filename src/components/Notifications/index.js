9c5f6033f6b4b1ca91aabb0e845ad40a5f7e2826
Emerson O da Hora <emersondahora@gmail.com>
2020-03-12 00:00:30 -0300

    Notificaçoes

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { MdNotifications } from "react-icons/md";
+import { parseISO, formatDistance } from "date-fns";
+import pt from "date-fns/locale/pt";
+
+import api from "~/service/api";
+
 import {
   Container,
   Badge,
@@ -8,34 +13,64 @@ import {
   Notification
 } from "./styles";
 
-const item = [1, 2, 3, 4, 5, 6];
 export default function Notifications() {
+  const [visible, setVisible] = useState(false);
+  const [notifications, setNotifications] = useState([]);
+
+  const hasUnread = useMemo(
+    () => !!notifications.find(notification => notification.read === false),
+    [notifications]
+  );
+
+  useEffect(() => {
+    async function loadNotifications() {
+      const response = await api.get("notifications");
+      const data = response.data.map(notification => ({
+        ...notification,
+        timeDistance: formatDistance(
+          parseISO(notification.createdAt),
+          new Date(),
+          { addSuffix: true, locale: pt }
+        )
+      }));
+      setNotifications(data);
+    }
+    loadNotifications();
+  }, []);
+
+  async function handleMarkAsRead(id) {
+    await api.put(`notifications/${id}`);
+
+    setNotifications(
+      notifications.map(notification =>
+        notification._id === id ? { ...notification, read: true } : notification
+      )
+    );
+  }
+  function handleToggleVisible() {
+    setVisible(!visible);
+  }
   return (
     <Container>
-      <Badge hasUnread>
+      <Badge hasUnread={hasUnread} onClick={handleToggleVisible}>
         <MdNotifications color="#7159c1" size={20} />
       </Badge>
 
-      <NotificationList>
+      <NotificationList visible={visible}>
         <Scrooll>
-          {item.map(() => (
-            <>
-              <Notification unread>
-                <p>Você possui um novo agendamento para amanhã</p>
-                <time>há 2 dias</time>
-                <button type="button">Marcar como lida</button>
-              </Notification>
-              <Notification>
-                <p>Você possui um novo agendamento para amanhã</p>
-                <time>há 2 dias</time>
-                <button type="button">Marcar como lida</button>
-              </Notification>
-              <Notification>
-                <p>Você possui um novo agendamento para amanhã</p>
-                <time>há 2 dias</time>
-                <button type="button">Marcar como lida</button>
-              </Notification>
-            </>
+          {notifications.map(notification => (
+            <Notification key={notification._id} unread={!notification.read}>
+              <p>{notification.content}</p>
+              <time>{notification.timeDistance}</time>
+              {!notification.read && (
+                <button
+                  type="button"
+                  onClick={() => handleMarkAsRead(notification._id)}
+                >
+                  Marcar como lida
+                </button>
+              )}
+            </Notification>
           ))}
         </Scrooll>
       </NotificationList>
--- src/components/Notifications/styles.js
+++ src/components/Notifications/styles.js
@@ -37,6 +37,7 @@ export const NotificationList = styled.div`
   background: rgba(0, 0, 0, 0.6);
   border-radius: 4px;
   padding: 15px 5px;
+  display: ${props => (props.visible ? "block" : "none")};
 
   &::before {
     content: "";
@@ -65,15 +66,14 @@ export const Notification = styled.div`
   time {
     font-size: 12px;
     opacity: 0.6;
+    display: block;
+    margin-bottom: 5px;
   }
   button {
     font-size: 12px;
     border: 0;
     background: none;
     color: ${lighten(0.2, "#7159c1")};
-    padding: 0 5px;
-    margin: 0 5px;
-    border-left: 1px solid rgba(255, 255, 255, 0.1);
   }
 
   ${props =>
@@ -86,6 +86,7 @@ export const Notification = styled.div`
         height: 8px;
         background: #ff892e;
         border-radius: 50%;
+        margin-left: 10px;
       }
     `}
 `;
