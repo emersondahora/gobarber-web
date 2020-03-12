cb40ec9eab468d740bfb48dc85fc7c77ae427430
Emerson O da Hora <emersondahora@gmail.com>
2020-03-12 11:12:05 -0300

    Foto do Perfil

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,3 +1,28 @@
-import { all } from "redux-saga/effects";
+import { all, takeLatest, call, put } from "redux-saga/effects";
+import { toast } from "react-toastify";
 
-export default all([]);
+import { updateProfileSuccess, updateProfileFailure } from "./actions";
+import api from "~/service/api";
+
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, avatar_id, ...rest } = payload.data;
+
+    const profile = {
+      name,
+      email,
+      avatar_id,
+      ...(rest.oldPassword ? rest : {})
+    };
+
+    const response = yield call(api.put, "users", profile);
+    toast.success("Perfil atualizado com sucesso.");
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (error) {
+    toast.error("Erro ao atualizar perfil, confira seus dados!");
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest("@user/UPDATE_PROFILE_REQUEST", updateProfile)]);
